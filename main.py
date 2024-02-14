47593562bd83ac8621f629cba3db3eb29f90ee40
Pedro Henrique Alves Lima <pedro-6-h@hotmail.com>
2024-02-14 09:14:16 -0300

    Mudança de pasta

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from model import Base, Vehicles
 from sqlalchemy.orm import Session
-from planets import *
-from people import *
-from films import *
-from ships import *
-from species import *
-from vehicles import *
+from scripts.planets import *
+from scripts.people import *
+from scripts.films import *
+from scripts.ships import *
+from scripts.species import *
+from scripts.vehicles import *
 
 engine = create_engine("sqlite:///SW.db", echo=True)
 Base.metadata.create_all(bind=engine)
